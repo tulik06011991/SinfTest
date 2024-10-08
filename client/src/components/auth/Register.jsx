0fab4d57b3760fc3d9824a789c81bc0769d032cf
tulik <tolqinmirsaliyev@gmail.com>
2024-10-08 13:04:05 -0500

    ssss

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
         e.preventDefault();
         try {
             await axios.post('http://localhost:5000/api/register', { name, email, password });
-            navigate('/login'); // Muvaffaqiyatli ro'yxatdan o'tgandan so'ng kirish sahifasiga o'tish
+            navigate('/'); // Muvaffaqiyatli ro'yxatdan o'tgandan so'ng kirish sahifasiga o'tish
         } catch (err) {
             setError('Ro\'yxatdan o\'tishda xatolik yuz berdi');
         }
