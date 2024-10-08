e14dd0e6d7c0451afe0f4a754bc33dfb8140fa3c
tulik <tolqinmirsaliyev@gmail.com>
2024-10-08 11:12:29 -0500

    sss

--- client/src/components/admin/SuperAdmin.jsx
+++ client/src/components/admin/SuperAdmin.jsx
@@ -68,7 +68,7 @@ const SuperadminPanel = () => {
               className={`${activeTab === 'categories' ? 'border-b-4 border-white' : ''} py-2 px-4`}
               onClick={() => setActiveTab('categories')}
             >
-              Fanlar Kategoriyasi
+              Fanlar
             </button>
           </Link>
         </div>
